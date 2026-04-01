91b74d5b5094e43d642a5b1e41e9c18972ac1330
rico-coder <rico_sonstiges@icloud.com>
2026-04-01 13:36:33 +0200

    feat: add match case for choice

--- bank_functions.py
+++ bank_functions.py
@@ -17,3 +17,20 @@ def get_summary(owner,**kwargs):
     print(f"\nAccount Summary for {owner}")
     for key, value in kwargs.items():
         print(f"  {key}: {value}")
+
+# function for choice
+def pick_choice(choice, balance):
+    match choice:
+        case "1":
+            amount = int(input("Deposit amount: "))
+            return deposit(balance, amount)
+        case "2":
+            amount = int(input("Withdraw amount: "))
+            return withdraw(balance, amount)
+        case "3":
+            return balance
+        case "4":
+            return None
+        case _:
+            print("Invalid choice.")
+            return balance
