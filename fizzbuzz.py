42e8c82fc090c7ea93fffb700c8b8f78036965f7
rico-coder <rico_sonstiges@icloud.com>
2026-04-01 11:44:51 +0200

    feat: add ternary function in print

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -12,3 +12,6 @@ def fizzbuzz(i):
             print(i)
 
 fizzbuzz(30)
+
+# Python ternary
+print([("FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)) for i in range(1, 31)])
